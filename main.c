466e0a6ddcc687bef922cb0c4ee7c226355bd126
bmuraya <babumuraya@gmail.com>
2023-04-24 21:47:30 +0300

    gcc wall

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
-int main(int ac, char **argv){
+int main(int ac, char **argv)
+{
   char *command_line = "(Demo_shell) $ ";
   char *lineptr;
   size_t n = 0; 
 
+ 
+ (void)ac; (void)argv;
+
   printf("%s", command_line);
   getline(&lineptr, &n, stdin);
+  printf("%s\n", lineptr);
 
   free(lineptr);
   return (0);
