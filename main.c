b0015ed35c3f4190ee520a775a21008104aec6be
bmuraya <babumuraya@gmail.com>
2023-04-24 21:15:24 +0300

    first complie

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
+int main(int ac, char **argv){
+  char *command_line = "(Demo_shell) $ ";
+  char *lineptr;
+  size_t n = 0; 
+
+  printf("%s", command_line);
+  getline(&lineptr, &n, stdin);
+
+  free(lineptr);
+  return (0);
+}
